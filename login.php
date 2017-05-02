82824f2b5ae33be01083045a633a2f44524b0c57
Mustafa <mustafa.hridoy505@gmail.com>
2017-05-02 13:46:07 +0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<!-- Favicon -->
+		<link rel="icon" href="#">
+
+		<!-- Page Title -->
+		<title>Byeah - Dahboard</title>
+
+		<!-- Global CSS [ Bootstrap, Font-Awesome, Animate CSS ] -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+		<!-- APP CSS -->
+		<link rel="stylesheet" href="assets/css/app.min.css">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+	</head>
+	<body>
+		
+		<div class="center-xy">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4 col-sm-offset-4">
+						
+						<div class="page-header text-center">
+							<h2>Byeah</h2>
+						</div>
+
+						<!-- Form - Login -->
+						<form action="#" method="POST" id="form-login" autocomplete="off">
+							<div class="panel panel-default">
+
+								<div class="panel-heading">
+									<h4>Dashboard | Log In</h4>
+								</div>
+
+								<div class="panel-body">
+
+									<div class="form-group">
+										<input type="text" name="username" placeholder="Username" class="form-control">
+									</div>
+								
+									<div class="form-group">
+										<input type="password" name="password" placeholder="Password" class="form-control">
+									</div>
+									
+									<div class="form-group">
+										<button type="submit" class="btn btn-sm btn-block btn-primary">Log In</button>
+									</div>
+
+								</div>
+
+							</div>
+
+							<a href="password-forgot.php">Forgot password?</a>
+
+						</form>
+						<!-- End Form - Login -->
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Global JS [  jQuery, Bootstrap ] -->
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+		<!-- APP JS -->
+		<script type="text/javascript" src="assets/js/app.min.js"></script>
+
+	</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,362 @@
+<div class="navbar-default sidebar" role="navigation">
+        
+    <!-- Sidebar Collapse -->
+    <div class="sidebar-nav navbar-collapse">
+        <ul class="nav" id="side-menu">
+            
+            <!-- Form - Sidebar Search -->
+            <li class="sidebar-search">
+                <div class="form-group">
+                    <div class="input-group custom-search-form">
+                        <input type="text" class="form-control" placeholder="Search...">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </span>
+                    </div>
+                </div>
+            </li>
+            <!-- Form - Sidebar Search -->
+            
+            <!-- Dashboard Links -->
+            <li>
+                <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+            </li>
+            <!-- #### -->
+            
+            <!-- Start Outreach Workshop -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-cogs fa-fw"></i> Outreach Workshop <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="outreach-data_entry.php">Enter Data</a>
+                    </li>
+                    <li>
+                        <a href="morris.html">Generate Reports <span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="outreach-generate_report-overall.php">Overall</a>
+                            </li>
+                            <li>
+                                <a href="outreach-generate_report-individual.php">Individual</a>
+                            </li>
+                            <li>
+                                <a href="outreach-generate_report-batch_wise.php">Batch-wise</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="outreach-analytics.php">Analytics <span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="outreach-generate_report-overall.php">KPI</a>
+                            </li>
+                            <li>
+                                <a href="outreach-generate_report-individual.php">Workshop</a>
+                            </li>
+                            <li>
+                                <a href="outreach-analytics_participents.php">Participants</a>
+                            </li>
+                            <li>
+                                <a href="outreach-generate_report-batch_wise.php">Elligible for Individual assesment</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+
+            <!-- Start Individusal Assement -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-user fa-fw"></i> Individual Assessment <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="individual_assessment-enter_data.php">Enter Data</a>
+                    </li>
+                    <li>
+                        <a href="#">Generate Reports<span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="Individusal_Assessment-Generate_Reports-overall.php">Overall</a>
+                            </li>
+                            <li>
+                                <a href="individual_assessment-generate_report-individual.php">Individual</a>
+                            </li>
+                            <li>
+                                <a href="#">Batch-wise</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="individusal_Assement-analytics.php">Analytics</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+            
+            <!-- Start Registration -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-user-plus fa-fw"></i> Registration <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="registration-enter_data.php">Enter Data</a>
+                    </li>
+                    <li>
+                        <a href="#">Generate Reports<span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="#">Overall</a>
+                            </li>
+                            <li>
+                                <a href="#">Individual</a>
+                            </li>
+                            <li>
+                                <a href="#">Batch-wise</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="registration-analytics.php">Analytics <span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="outreach-generate_report-overall.php">KPI</a>
+                            </li>
+                            <li>
+                                <a href="outreach-generate_report-individual.php">Individusal Assessment</a>
+                            </li>
+                            <li>
+                                <a href="outreach-generate_report-batch_wise.php">Participants</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+            
+            <!-- Start Ell Data -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-database fa-fw"></i> Ell Data <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="#">Enter Data<span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="ell_data-enter_data-enter_baseline_data.php">Enter baseline Data</a>
+                            </li>
+                            <li>
+                                <a href="ell_data-analytics-how_i_am.php">How I Am</a>
+                            </li>
+                            <li>
+                                <a href="#">What I Know</a>
+                            </li>
+                            <li>
+                                <a href="ell_data-analytics-Who_i_know.php">Who I Know</a>
+                            </li>
+                            <li>
+                                <a href="ell_data-analytics-what_i_earn.php">What I Earn </a>
+                            </li>
+                            <li>
+                                <a href="#">Enter Midline Data </a>
+                            </li>
+                            <li>
+                                <a href="#">Enter Endline Data </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Generate Reports<span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="#">Overall</a>
+                            </li>
+                            <li>
+                                <a href="#">Individual</a>
+                            </li>
+                            <li>
+                                <a href="#">Batch-wise</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Analytics <span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="ell_data-analytics-how_i_am.php">How I Am</a>
+                            </li>
+                            <li>
+                                <a href="#">What I Know</a>
+                            </li>
+                            <li>
+                                <a href="ell_data-analytics-Who_i_know.php">How I Know</a>
+                            </li>
+                            <li>
+                                <a href="ell_data-analytics-what_i_earn.php">What I Earn </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+
+            <!-- Start Mentorship -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-wrench fa-fw"></i> Mentorship <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="mentorship-recruitment_and_registration.php">Recruitment and Registration</a>
+                    </li>
+                    <li>
+                        <a href="mentorship-orientatio.php">Orientation</a>
+                    </li>
+                    <li>
+                        <a href="mentorship-matching_workshop.php">Matching Workshop</a>
+                    </li>
+                    <li>
+                        <a href="mentorship-mentorin_club.php">Mentoring Club</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+            
+            <!-- Start Basic Traning -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-language fa-fw"></i> Basic Training <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="#">
+                            <i class="fa-fw"></i> Enter Traning Data <span class="fa arrow"></span>
+                        </a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="basic_traning-enter_traning_data-pre_test_score.php">Pre-Test Score</a>
+                            </li>
+                            <li>
+                                <a href="basic_traning-enter_traning_data-post_test_score.php">Post-Test Score</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="basic_traning-analytics.php">Analytics</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+
+            <!-- Start Advance Traning -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-users fa-fw"></i> Advance Training <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="#">
+                            <i class="fa-fw"></i>Enter Traning Data<span class="fa arrow"></span>
+                        </a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="advance_traning-enter_traning_data-pre_test_score.php">Pre-Test Score</a>
+                            </li>
+                            <li>
+                                <a href="advance_traning-enter_traning_data-post_test_score.php">Post-Test Score</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="advance_traning-analytics.php">Analytics</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+            
+            <!-- Start Business Plan Lab -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-thermometer-full fa-fw"></i> Business Plan Lab <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="business_plan_lab-participant.php">Participant</a>
+                    </li>
+                    <li>
+                        <a href="business_plan_lab-admin.php">Admin</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+            
+            <!-- Start Business Plan Trial -->
+            <li>
+                <a href="business_plan_trial.php">
+                    <i class="fa fa-gavel fa-fw"></i> Business Plan Trial 
+                </a>
+            </li>
+            <!-- #### -->
+            
+            <!-- Start Loan Panel Review -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-list fa-fw"></i> Loan Panel Review <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="loan_panel_review-review_schedule.php">Review Schedule</a>
+                    </li>
+                    <li>
+                        <a href="loan_panel_review-show-calendar.php">Show Calender</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+            
+            <!-- Start FI / NBFI Orientation -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-handshake-o fa-fw"></i> FI / NBFI Orientation <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="nbfi-calender_entry.php">Calender Entry</a>
+                    </li>
+                    <li>
+                        <a href="nbfi-show_calender.php">Show Calender</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+            
+            <!-- Start Follow Up -->
+            <li>
+                <a href="#">
+                    <i class="fa fa-map-signs fa-fw"></i> Follow Up <span class="fa arrow"></span>
+                </a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="follow_up-calender_entry.php">Calender Entry</a>
+                    </li>
+                    <li>
+                        <a href="follow_up-show_calender.php">Show Calender</a>
+                    </li>
+                </ul>
+            </li>
+            <!-- #### -->
+
+            <!-- End Dashboard Links -->
+
+        </ul>
+    </div>
+    <!-- Sidebar Collapse -->
+
+</div>
